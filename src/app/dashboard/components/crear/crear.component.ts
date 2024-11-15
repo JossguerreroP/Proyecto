98ed254681e204d001d270806c1f19d1ac62684d
JossguerreroP <josguerrerop@udistrital.edu.co>
2024-11-15 07:09:55 -0500

    ok

--- src/app/dashboard/components/crear/crear.component.ts
+++ src/app/dashboard/components/crear/crear.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AutoScrollService, ColDef, SizeColumnsToFitGridStrategy } from 'ag-grid-community';
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-crear',
@@ -8,7 +8,34 @@ import { AutoScrollService, ColDef, SizeColumnsToFitGridStrategy } from 'ag-grid
   styleUrl: './crear.component.css',
  
 })
-export class CrearComponent {
+export class CrearComponent implements OnInit {
+
+  sd:boolean = false;
+  st:boolean = true;
+  
+
+  constructor(private route:ActivatedRoute) 
+  { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+
+    if(params['sd']){
+      this.sd =true;
+      this.st =false;
+    }
+
+    if(params['st']){
+      this.st=true;
+      this.sd =false;
+    }
+     
+    })
+     
+   }
+
+Objects:Object[]=[]
+  generadores :any;
 
   autoSizeStrategy :SizeColumnsToFitGridStrategy = {
     type: 'fitGridWidth',
@@ -21,52 +48,68 @@ export class CrearComponent {
     ]
 };
 
-  rowData : Object[]=[{Titulo:234,Idioma:"df"}]
+  rowData : Object[]=[]
 
   colDefs: ColDef[] = [
     { 
       field: "NODO", 
       headerName: 'Nodo',
-    
-      
     },
     { 
-      field: "PG(MW)", 
+      field: "PG_MW", 
       headerName: 'PG (MW)', 
       type: 'numericColumn' 
     },
     { 
-      field: "QG(MW)", 
+      field: "QG_MW", 
       headerName: 'QG (MW)', 
       type: 'numericColumn' 
     },
     { 
-      field: "Q_MAX(MVAR)", 
+      field: "Q_MAX_MVAR", 
       headerName: 'Q_MAX (MVAR)', 
       type: 'numericColumn' 
     },
     { 
-      field: "Q_MIN(MVAR)", 
+      field: "Q_MIN_MVAR", 
       headerName: 'Q_MIN (MVAR)', 
       type: 'numericColumn' 
     },
     { 
-      field: "P_MAX(MW)", 
+      field: "P_MAX_MW", 
       headerName: 'P_MAX (MW)', 
       type: 'numericColumn' 
     },
     { 
-      field: "COSTO_MARGINAL($/MW)", 
+      field: "costo_marginal_dolar_per_MW", 
       headerName: 'Costo Marginal ($/MW)', 
       type: 'numericColumn', 
-      valueFormatter: (params) => `$${params.value.toFixed(2)}`  // Formato de precio
+      //valueFormatter: (params) => `$${params.value.toFixed(2)}`  // Formato de precio
     },
     
   ];
 
 
 getEntradasIniciales(event: any) {
-  console.log(event)
+  this.generadores=event.generadores
+  console.log(this.generadores.generators)
+  const newRows: object[] = [];  
+
+  for (let i = 0; i < this.generadores.generators.length; i++) {
+    let generador:Object ={
+      NODO: this.generadores.generators[i].nodo,
+      PG_MW: this.generadores.generators[i].PG_MW,
+      QG_MW: this.generadores.generators[i].QG_MW,
+      Q_MAX_MVAR: this.generadores.generators[i].Q_MAX_MVAR,
+      Q_MIN_MVAR: this.generadores.generators[i].Q_MIN_MVAR,
+      P_MAX_MW: this.generadores.generators[i].P_MAX_MW,
+      costo_marginal_dolar_per_MW: this.generadores.generators[i].costo_marginal_dolar_per_MW
+    }
+    newRows.push(generador);
+}
+this.rowData = newRows;
+console.log(this.rowData)
+}
 }
 
-}
+
--- src/app/dashboard/components/wrapper/wrapper.component.ts
+++ src/app/dashboard/components/wrapper/wrapper.component.ts
@@ -2,28 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
+
 @Component({
   selector: 'app-wrapper',
   templateUrl: './wrapper.component.html',
   styleUrl: './wrapper.component.css'
 })
 export class WrapperComponent implements OnInit {
-  expanded:boolean = false;
+  selected:boolean =false;
+  expanded:boolean = true;
   private data:any;
   public  user:any=true;
 
-  constructor(private dialog: MatDialog, private router:Router,) 
+  constructor(private dialog: MatDialog, private router:Router) 
   { }
  
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+   
   }
 
   expander(){
-    if(!this.expanded){
-      this.expanded=true;
-    }else{
+    if(this.expanded){
       this.expanded=false;
+    }else{
+      this.expanded=true;
       
     }
     console.log(this.expanded)
